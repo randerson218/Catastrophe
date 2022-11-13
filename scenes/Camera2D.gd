a872f783c63974082479e563d633d61cc30d91ba
Richard <richande218@gmail.com>
2022-11-13 13:52:09 -0600

    Added zoom to hook camera

--- scenes/Camera2D.gd
+++ scenes/Camera2D.gd
@@ -3,10 +3,21 @@ extends Camera2D
 export (NodePath) var TargetNodepath = null
 var target_node
 export (float) var lerpspeed = 0.05
+export (float) var zoomspeed = 0.05
+var is_focused_on_hook
  
 func _ready():
 		target_node  = get_node(TargetNodepath)
 		self.position = target_node.position
+		self.zoom = Vector2(0.5,0.5)
+
 func _process(delta):
- 
+	is_focused_on_hook = target_node == get_node("../Hook")
+	if is_focused_on_hook:
+		self.zoom = Vector2(lerp(self.zoom.x,0.25,zoomspeed),lerp(self.zoom.y,0.25,zoomspeed))
+	else:
+		self.zoom = Vector2(lerp(self.zoom.x,0.5,zoomspeed),lerp(self.zoom.y,0.5,zoomspeed))
+	
+	
 	self.position = lerp(self.position, target_node.position, lerpspeed)
+	
