b36a145831969249d0946e2748c92b699e12603a
HohoKatze <aahoheisel2@gmail.com>
2022-11-13 19:30:41 -0600

    Added map buttons, levels, and made it look better

--- scenes/Map.gd
+++ scenes/Map.gd
@@ -11,3 +11,19 @@ func set_is_paused(value):
 	is_paused = value
 	get_tree().paused = is_paused
 	visible = is_paused
+
+
+func _on_CaveButton_pressed():
+	get_tree().change_scene("res://scenes/SecondLevel.tscn")
+	self.is_paused = false
+
+
+
+func _on_DesetButton_pressed():
+	get_tree().change_scene("res://scenes/ThirdLevel.tscn")
+	self.is_paused = false
+
+
+func _on_ForestButton_pressed():
+	get_tree().change_scene("res://scenes/FirstLevel.tscn")
+	self.is_paused = false
