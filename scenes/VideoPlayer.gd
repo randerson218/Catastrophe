fdff3345548ab41f5d141fc2b24a30a3611bd7a0
HohoKatze <aahoheisel2@gmail.com>
2022-11-30 00:00:11 -0600

    Created Main Menu
    Added space video
    Added rocket ship

---
+++ scenes/VideoPlayer.gd
@@ -0,0 +1,24 @@
+extends VideoPlayer
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	play() # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if !is_playing():
+		play()
+	
+	
+	
+	
+	
+	
+
